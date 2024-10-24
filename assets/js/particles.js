4b0a251096351d1c337d5eb3263842b478ecd92c
cshmark <168860117+markie1111@users.noreply.github.com>
2024-10-24 22:25:47 +0300

    Update particles.js
    FASZ

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -48,7 +48,7 @@ particlesJS("particles-js", {
         "line_linked": {
             "enable": true,
             "distance": 150,
-            "color": "090c9b",
+            "color": "#1100ff",
             "opacity": 1,
             "width": 1
         },
